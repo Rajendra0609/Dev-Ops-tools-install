b9bd1117bdcba441dd2199005b8c445c7c22ea7b
Rajendra0609 <106649883+Rajendra0609@users.noreply.github.com>
2025-06-12 11:59:00 +0530

    Update Dev-Ops-tools_install.sh
    Here is a cross-distro (Ubuntu/Debian, CentOS/RHEL, Fedora) version of your DevOps tools installer script.

--- Dev-Ops-tools_install.sh
+++ Dev-Ops-tools_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status
+set -e
 
-# Constants for tool versions (updated as of June 2025)
+# Tool Versions (as of June 2025)
 KUBECTL_VERSION="$(curl -s https://dl.k8s.io/release/stable.txt)"
 TERRAFORM_VERSION="1.8.5"
 JENKINS_REPO="https://pkg.jenkins.io"
@@ -10,23 +10,25 @@ AWS_CLI_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 GOOGLE_CLOUD_SDK_URL="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-474.0.0-linux-x86_64.tar.gz"
 HELM_SCRIPT="https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3"
 GRAFANA_DEB_URL="https://dl.grafana.com/oss/release/grafana_11.0.0_amd64.deb"
+GRAFANA_RPM_URL="https://dl.grafana.com/oss/release/grafana-11.0.0-1.x86_64.rpm"
+PROMETHEUS_VERSION="2.52.0"
+NODE_EXPORTER_VERSION="1.8.1"
+MAVEN_VERSION="3.9.7"
+GRADLE_VERSION="8.7"
+ZAP_VERSION="2.15.0"
 PACKER_VERSION="1.10.2"
 VAGRANT_VERSION="2.4.1"
 ISTIO_VERSION="1.23.2"
 
-# Welcome Message
 clear
 echo "############################################################################"
-echo "#                                                                          #"
 echo "#          DevOps Tool Installer/Uninstaller by RajaChowdary tech          #"
-echo "#                                                                          #"
 echo "############################################################################"
 echo ""
 echo "Automate the installation and uninstallation of essential DevOps tools on your Linux machine."
 echo "Choose from a wide range of tools and get started quickly and easily."
 echo ""
 
-# Function to prompt user for their Linux distribution
 get_linux_distribution() {
   echo "Select your Linux distribution:"
   echo "1. Ubuntu/Debian"
@@ -42,39 +44,85 @@ get_linux_distribution() {
   esac
 }
 
-# Function to handle installations
 install_tool() {
   local tool_name="$1"
   local install_cmd="$2"
-
   echo "Installing $tool_name..."
   eval "$install_cmd"
   echo "$tool_name installed successfully."
 }
 
-# Function to handle uninstallation
 uninstall_tool() {
   local tool_name="$1"
   local uninstall_cmd="$2"
-
   echo "Uninstalling $tool_name..."
   eval "$uninstall_cmd"
   echo "$tool_name uninstalled successfully."
 }
 
-# Installation Commands (Ubuntu/Debian-centric, update as needed for other distros)
-declare -A install_commands=(
+declare -A install_commands_ubuntu=(
   [docker]="sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg && sudo install -m 0755 -d /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg && echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null && sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io"
-  [kubectl]="curl -LO https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/"
+  [grafana]="wget $GRAFANA_DEB_URL && sudo dpkg -i $(basename $GRAFANA_DEB_URL) && sudo apt-get install -f -y && rm $(basename $GRAFANA_DEB_URL)"
+  [jenkins]="curl -fsSL $JENKINS_REPO/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null && echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] $JENKINS_REPO/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null && sudo apt-get update && sudo apt-get install -y fontconfig openjdk-17-jre jenkins"
   [ansible]="sudo apt-get update && sudo apt-get install -y ansible"
-  [terraform]="curl -LO https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip && sudo mv terraform /usr/local/bin/ && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
-  [jenkins]="curl -fsSL $JENKINS_REPO/debian/jenkins.io-2023.key | sudo tee \
-    /usr/share/keyrings/jenkins-keyring.asc > /dev/null && echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] $JENKINS_REPO/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null && sudo apt-get update && sudo apt-get install -y fontconfig openjdk-17-jre jenkins"
-  [awscli]="curl \"$AWS_CLI_URL\" -o \"awscliv2.zip\" && unzip awscliv2.zip && sudo ./aws/install && rm -rf awscliv2.zip aws/"
+  [lynis]="sudo apt-get update && sudo apt-get install -y lynis"
   [azurecli]="curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash"
+)
+declare -A uninstall_commands_ubuntu=(
+  [docker]="sudo apt-get remove -y docker-ce docker-ce-cli containerd.io && sudo apt-get purge -y docker-ce docker-ce-cli containerd.io && sudo rm -rf /var/lib/docker"
+  [grafana]="sudo apt-get remove -y grafana && sudo apt-get purge -y grafana"
+  [jenkins]="sudo apt-get remove -y jenkins && sudo apt-get purge -y jenkins"
+  [ansible]="sudo apt-get remove -y ansible && sudo apt-get purge -y ansible"
+  [lynis]="sudo apt-get remove -y lynis && sudo apt-get purge -y lynis"
+  [azurecli]="sudo apt-get remove -y azure-cli && sudo apt-get purge -y azure-cli"
+)
+
+declare -A install_commands_centos=(
+  [docker]="sudo yum install -y yum-utils && sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && sudo yum install -y docker-ce docker-ce-cli containerd.io && sudo systemctl enable --now docker"
+  [grafana]="wget $GRAFANA_RPM_URL && sudo yum install -y $(basename $GRAFANA_RPM_URL) && rm $(basename $GRAFANA_RPM_URL)"
+  [jenkins]="sudo yum install -y fontconfig java-17-openjdk && sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo && sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key && sudo yum install -y jenkins && sudo systemctl enable --now jenkins"
+  [ansible]="sudo yum install -y epel-release && sudo yum install -y ansible"
+  [lynis]="sudo yum install -y epel-release && sudo yum install -y lynis"
+  [azurecli]="sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && sudo sh -c 'echo -e \"[azure-cli]\\nname=Azure CLI\\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\\nenabled=1\\ngpgcheck=1\\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\" > /etc/yum.repos.d/azure-cli.repo' && sudo yum install -y azure-cli"
+)
+declare -A uninstall_commands_centos=(
+  [docker]="sudo yum remove -y docker-ce docker-ce-cli containerd.io && sudo rm -rf /var/lib/docker"
+  [grafana]="sudo yum remove -y grafana"
+  [jenkins]="sudo yum remove -y jenkins"
+  [ansible]="sudo yum remove -y ansible"
+  [lynis]="sudo yum remove -y lynis"
+  [azurecli]="sudo yum remove -y azure-cli"
+)
+
+declare -A install_commands_fedora=(
+  [docker]="sudo dnf -y install dnf-plugins-core && sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo && sudo dnf install -y docker-ce docker-ce-cli containerd.io && sudo systemctl enable --now docker"
+  [grafana]="wget $GRAFANA_RPM_URL && sudo dnf install -y $(basename $GRAFANA_RPM_URL) && rm $(basename $GRAFANA_RPM_URL)"
+  [jenkins]="sudo dnf install -y fontconfig java-17-openjdk && sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo && sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key && sudo dnf install -y jenkins && sudo systemctl enable --now jenkins"
+  [ansible]="sudo dnf install -y ansible"
+  [lynis]="sudo dnf install -y lynis"
+  [azurecli]="sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && sudo sh -c 'echo -e \"[azure-cli]\\nname=Azure CLI\\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\\nenabled=1\\ngpgcheck=1\\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\" > /etc/yum.repos.d/azure-cli.repo' && sudo dnf install -y azure-cli"
+)
+declare -A uninstall_commands_fedora=(
+  [docker]="sudo dnf remove -y docker-ce docker-ce-cli containerd.io && sudo rm -rf /var/lib/docker"
+  [grafana]="sudo dnf remove -y grafana"
+  [jenkins]="sudo dnf remove -y jenkins"
+  [ansible]="sudo dnf remove -y ansible"
+  [lynis]="sudo dnf remove -y lynis"
+  [azurecli]="sudo dnf remove -y azure-cli"
+)
+
+# Cross-distro (binary) installs
+declare -A binary_installs=(
+  [kubectl]="curl -LO https://dl.k8s.io/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl && chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/"
+  [terraform]="curl -LO https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip && sudo mv terraform /usr/local/bin/ && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+  [awscli]="curl \"$AWS_CLI_URL\" -o \"awscliv2.zip\" && unzip awscliv2.zip && sudo ./aws/install && rm -rf awscliv2.zip aws/"
   [gcloud]="curl -O $GOOGLE_CLOUD_SDK_URL && tar -xvzf google-cloud-sdk-474.0.0-linux-x86_64.tar.gz && ./google-cloud-sdk/install.sh"
   [helm]="curl -fsSL -o get_helm.sh $HELM_SCRIPT && chmod 700 get_helm.sh && ./get_helm.sh"
-  [grafana]="wget $GRAFANA_DEB_URL && sudo dpkg -i $(basename $GRAFANA_DEB_URL) && rm $(basename $GRAFANA_DEB_URL)"
+  [prometheus]="curl -LO https://github.com/prometheus/prometheus/releases/download/v${PROMETHEUS_VERSION}/prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz && tar xvf prometheus-${PROMETHEUS_VERSION}.linux-amd64.tar.gz && sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/prometheus /usr/local/bin/ && sudo mv prometheus-${PROMETHEUS_VERSION}.linux-amd64/promtool /usr/local/bin/ && rm -rf prometheus-${PROMETHEUS_VERSION}.linux-amd64*"
+  [node_exporter]="curl -LO https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz && tar xvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz && sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin/ && rm -rf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64*"
+  [maven]="curl -LO https://dlcdn.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz && sudo tar xzf apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt && sudo ln -sf /opt/apache-maven-${MAVEN_VERSION}/bin/mvn /usr/local/bin/mvn && rm apache-maven-${MAVEN_VERSION}-bin.tar.gz"
+  [gradle]="curl -LO https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-bin.zip && sudo unzip gradle-${GRADLE_VERSION}-bin.zip -d /opt && sudo ln -sf /opt/gradle-${GRADLE_VERSION}/bin/gradle /usr/local/bin/gradle && rm gradle-${GRADLE_VERSION}-bin.zip"
+  [oswap]="wget https://github.com/zaproxy/zaproxy/releases/download/v${ZAP_VERSION}/ZAP_${ZAP_VERSION}_Linux.tar.gz && tar -xzf ZAP_${ZAP_VERSION}_Linux.tar.gz && sudo mv ZAP_${ZAP_VERSION} /opt/ && sudo ln -sf /opt/ZAP_${ZAP_VERSION}/zap.sh /usr/local/bin/zap && rm ZAP_${ZAP_VERSION}_Linux.tar.gz"
   [gitlab-runner]="sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64 && sudo chmod +x /usr/local/bin/gitlab-runner"
   [vault]="curl -LO https://releases.hashicorp.com/vault/1.16.2/vault_1.16.2_linux_amd64.zip && unzip vault_1.16.2_linux_amd64.zip && sudo mv vault /usr/local/bin/ && rm vault_1.16.2_linux_amd64.zip"
   [consul]="curl -LO https://releases.hashicorp.com/consul/1.17.2/consul_1.17.2_linux_amd64.zip && unzip consul_1.17.2_linux_amd64.zip && sudo mv consul /usr/local/bin/ && rm consul_1.17.2_linux_amd64.zip"
@@ -83,21 +131,19 @@ declare -A install_commands=(
   [minikube]="curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64"
   [packer]="curl -LO https://releases.hashicorp.com/packer/$PACKER_VERSION/packer_$PACKER_VERSION_linux_amd64.zip && unzip packer_$PACKER_VERSION_linux_amd64.zip && sudo mv packer /usr/local/bin/ && rm packer_$PACKER_VERSION_linux_amd64.zip"
   [vagrant]="curl -LO https://releases.hashicorp.com/vagrant/$VAGRANT_VERSION/vagrant_${VAGRANT_VERSION}_linux_amd64.zip && unzip vagrant_${VAGRANT_VERSION}_linux_amd64.zip && sudo mv vagrant /usr/local/bin/ && rm vagrant_${VAGRANT_VERSION}_linux_amd64.zip"
-  [lynis]="sudo apt-get update && sudo apt-get install -y lynis"
 )
 
-# Uninstallation Commands
-declare -A uninstall_commands=(
-  [docker]="sudo apt-get remove -y docker-ce docker-ce-cli containerd.io && sudo apt-get purge -y docker-ce docker-ce-cli containerd.io && sudo rm -rf /var/lib/docker"
+declare -A binary_uninstalls=(
   [kubectl]="sudo rm /usr/local/bin/kubectl"
-  [ansible]="sudo apt-get remove -y ansible && sudo apt-get purge -y ansible"
   [terraform]="sudo rm /usr/local/bin/terraform"
-  [jenkins]="sudo apt-get remove -y jenkins && sudo apt-get purge -y jenkins"
   [awscli]="sudo rm /usr/local/bin/aws && sudo rm -rf /usr/local/aws-cli"
-  [azurecli]="sudo apt-get remove -y azure-cli && sudo apt-get purge -y azure-cli"
   [gcloud]="sudo rm -rf google-cloud-sdk"
   [helm]="sudo rm /usr/local/bin/helm"
-  [grafana]="sudo apt-get remove -y grafana && sudo apt-get purge -y grafana"
+  [prometheus]="sudo rm /usr/local/bin/prometheus /usr/local/bin/promtool"
+  [node_exporter]="sudo rm /usr/local/bin/node_exporter"
+  [maven]="sudo rm /usr/local/bin/mvn && sudo rm -rf /opt/apache-maven-*"
+  [gradle]="sudo rm /usr/local/bin/gradle && sudo rm -rf /opt/gradle-*"
+  [oswap]="sudo rm /usr/local/bin/zap && sudo rm -rf /opt/ZAP_*"
   [gitlab-runner]="sudo rm /usr/local/bin/gitlab-runner"
   [vault]="sudo rm /usr/local/bin/vault"
   [consul]="sudo rm /usr/local/bin/consul"
@@ -106,12 +152,9 @@ declare -A uninstall_commands=(
   [minikube]="sudo rm /usr/local/bin/minikube"
   [packer]="sudo rm /usr/local/bin/packer"
   [vagrant]="sudo rm /usr/local/bin/vagrant"
-  [lynis]="sudo apt-get remove -y lynis && sudo apt-get purge -y lynis"
 )
 
-# Prompt user for action
 read -p "Do you want to install or uninstall tools? (install/uninstall): " action
-
 if [[ "$action" != "install" && "$action" != "uninstall" ]]; then
   echo "Invalid action selected. Exiting."
   exit 1
@@ -119,26 +162,45 @@ fi
 
 get_linux_distribution
 
-# Prompt user for tool selection
 echo "Select tools to $action (separate with spaces):"
-for tool in "${!install_commands[@]}"; do
+for tool in "${!install_commands_ubuntu[@]}" "${!binary_installs[@]}"; do
   echo "- $tool"
 done
 read -p "Your selection: " selected_tools
 
-# Process each selected tool
 for tool in $selected_tools; do
+  # Distro-specific installs
   if [[ "$action" == "install" ]]; then
-    if [[ -v install_commands["$tool"] ]]; then
-      install_tool "$tool" "${install_commands[$tool]}"
-    else
-      echo "No installation command found for $tool."
+    case "$DISTRO" in
+      ubuntu)
+        [[ -v install_commands_ubuntu["$tool"] ]] && install_tool "$tool" "${install_commands_ubuntu[$tool]}"
+        ;;
+      centos)
+        [[ -v install_commands_centos["$tool"] ]] && install_tool "$tool" "${install_commands_centos[$tool]}"
+        ;;
+      fedora)
+        [[ -v install_commands_fedora["$tool"] ]] && install_tool "$tool" "${install_commands_fedora[$tool]}"
+        ;;
+    esac
+    # Binary cross-distro tools
+    if [[ -v binary_installs["$tool"] ]]; then
+      install_tool "$tool" "${binary_installs[$tool]}"
     fi
   elif [[ "$action" == "uninstall" ]]; then
-    if [[ -v uninstall_commands["$tool"] ]]; then
-      uninstall_tool "$tool" "${uninstall_commands[$tool]}"
-    else
-      echo "No uninstallation command found for $tool."
+    case "$DISTRO" in
+      ubuntu)
+        [[ -v uninstall_commands_ubuntu["$tool"] ]] && uninstall_tool "$tool" "${uninstall_commands_ubuntu[$tool]}"
+        ;;
+      centos)
+        [[ -v uninstall_commands_centos["$tool"] ]] && uninstall_tool "$tool" "${uninstall_commands_centos[$tool]}"
+        ;;
+      fedora)
+        [[ -v uninstall_commands_fedora["$tool"] ]] && uninstall_tool "$tool" "${uninstall_commands_fedora[$tool]}"
+        ;;
+    esac
+    # Binary cross-distro tools
+    if [[ -v binary_uninstalls["$tool"] ]]; then
+      uninstall_tool "$tool" "${binary_uninstalls[$tool]}"
     fi
   fi
 done
