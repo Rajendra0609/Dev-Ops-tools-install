b3f651d96f7d83a0c2a3a4387da1ac9ba4fcc114
Rajendra0609 <106649883+Rajendra0609@users.noreply.github.com>
2025-08-08 15:03:39 +0530

    Update kubernetes_install.sh

--- kubernetes_install.sh
+++ kubernetes_install.sh
@@ -53,9 +53,9 @@ EOF
 
 # Function to install Kubernetes components
 install_kubernetes() {
-    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+    curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33.3/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+    echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33.3/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
     sudo apt-get update
     sudo apt-get install -y kubelet kubeadm kubectl
